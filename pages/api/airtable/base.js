15cb6aafdc1918e52a02c859be183390dff04cf2
renatodex <renatodex@gmail.com>
2020-05-30 20:53:56 -0300

    Add console

--- pages/api/airtable/base.js
+++ pages/api/airtable/base.js
@@ -3,6 +3,8 @@ export async function list (resource, options = {}) {
   const APP_ID = process.env.AIRTABLE_APP_ID
   const API_KEY = process.env.AIRTABLE_API_KEY
 
+  console.log(BASE_URL)
+
   const data = await fetch(
     `${BASE_URL}/${APP_ID}/${resource}?maxRecords=${options.maxRecords || 3}&view=${options.view || 'Grid%20view'}`,
     { headers: { 'Authorization': `Bearer ${API_KEY}` }
