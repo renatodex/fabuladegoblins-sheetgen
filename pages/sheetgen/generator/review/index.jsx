d8efcbe27f0b07c5cc72302484455b4fe7f5ca81
renatodex <renatodex@gmail.com>
2020-06-03 04:04:33 -0300

    Testing build

--- pages/sheetgen/generator/review/index.jsx
+++ pages/sheetgen/generator/review/index.jsx
@@ -2,43 +2,54 @@ import styles from './style.module.scss'
 import { store } from 'modules/redux_store'
 import { useEffect } from 'react'
 
-export function getProps() {
-  let sheetData = store.getState().sheet_data
-  return {
-    ...sheetData
-  }
-}
+// export function getProps() {
+//   let sheetData = store.getState().sheet_data
+//   return {
+//     ...sheetData
+//   }
+// }
 
 export default function () {
-  if (process.env.NEXT_PUBLIC_SKIP_STEPS && !store.getState().sheet_data.character_name) {
-    Router.push('/sheetgen/generator')
-  }
-
-  let props = getProps()
+  let attributes = {}
+  let props = {}
+  let hpPoints
+  let mpPoints
+  let attackPoints
+  let defensePoints
 
   let calculateModifier = function (amount) {
     return Math.floor(amount / 3) - 2
   }
 
-  let attributes = props.attributes_data
-  let selectedWeapon = props.selected_weapon
-  let selectedWeaponAttribute = props.selected_weapon?.attribute__handle?.[0]
-  let selectedArmor = props.selected_armor
-
-  let initialHp = parseInt(props.class_data?.initial_hp)
-  let initialMp = parseInt(props.class_data?.initial_mp)
-
-  let hpPoints = initialHp + parseInt(attributes?.strength)
-  let mpPoints = initialMp + parseInt(attributes?.intelligence)
-
-  let strength = parseInt(attributes?.strength)
-  let agility = parseInt(attributes?.agility)
-
-  let weaponAttackBonus = parseInt(selectedWeapon?.attack_bonus) || 0
-  let armorDefenseBonus = parseInt(selectedArmor?.defense_bonus) || 0
-
-  let attackPoints = 10 + calculateModifier(parseInt(attributes[selectedWeaponAttribute])) + weaponAttackBonus
-  let defensePoints = 10 + calculateModifier(Math.max(strength, agility)) + armorDefenseBonus
+  //
+  // if (process.browser) {
+  //   if (process.env.NEXT_PUBLIC_SKIP_STEPS && !store.getState().sheet_data.character_name) {
+  //     Router.push('/sheetgen/generator')
+  //   }
+  //
+  //   props = getProps()
+  //
+  //
+  //   attributes = props.attributes_data
+  //   let selectedWeapon = props.selected_weapon
+  //   let selectedWeaponAttribute = props.selected_weapon?.attribute__handle?.[0]
+  //   let selectedArmor = props.selected_armor
+  //
+  //   let initialHp = parseInt(props.class_data?.initial_hp)
+  //   let initialMp = parseInt(props.class_data?.initial_mp)
+  //
+  //   let hpPoints = initialHp + parseInt(attributes?.strength)
+  //   let mpPoints = initialMp + parseInt(attributes?.intelligence)
+  //
+  //   let strength = parseInt(attributes?.strength)
+  //   let agility = parseInt(attributes?.agility)
+  //
+  //   let weaponAttackBonus = parseInt(selectedWeapon?.attack_bonus) || 0
+  //   let armorDefenseBonus = parseInt(selectedArmor?.defense_bonus) || 0
+  //
+  //   let attackPoints = 10 + calculateModifier(parseInt(attributes[selectedWeaponAttribute])) + weaponAttackBonus
+  //   let defensePoints = 10 + calculateModifier(Math.max(strength, agility)) + armorDefenseBonus
+  // }
 
   return (
     <div>
@@ -47,12 +58,12 @@ export default function () {
       <div className={styles['review_blocks']}>
         <div className={styles['review_block']}>
           <h2 className={styles['title']}>Nome do Personagem</h2>
-          <p>{ props.character_name }</p>
+          <p>{ props?.character_name }</p>
         </div>
 
         <div className={styles['review_block']}>
           <h2 className={styles['title']}>Nome do Jogador</h2>
-          <p>{ props.player_name }</p>
+          <p>{ props?.player_name }</p>
         </div>
 
         <div className={styles['review_block']}>
@@ -72,19 +83,19 @@ export default function () {
             ].join(' ')
         }>
           <div className={styles["attribute_block"]}>
-            <h2 className={styles["attribute-label"]}>Força <span>{attributes?.strength}</span> ({ calculateModifier(attributes?.strength) })</h2>
+            <h2 className={styles["attribute-label"]}>Força <span>{attributes?.strength}</span> ({ calculateModifier(attributes?.strength || 0) })</h2>
           </div>
           <div className={styles["attribute_block"]}>
-            <h2 className={styles["attribute-label"]}>Agilidade <span>{attributes?.agility}</span> ({ calculateModifier(attributes?.agility) })</h2>
+            <h2 className={styles["attribute-label"]}>Agilidade <span>{attributes?.agility}</span> ({ calculateModifier(attributes?.agility || 0) })</h2>
           </div>
           <div className={styles["attribute_block"]}>
-            <h2 className={styles["attribute-label"]}>Inteligencia <span>{attributes?.intelligence}</span> ({ calculateModifier(attributes?.intelligence) })</h2>
+            <h2 className={styles["attribute-label"]}>Inteligencia <span>{attributes?.intelligence}</span> ({ calculateModifier(attributes?.intelligence || 0) })</h2>
           </div>
           <div className={styles["attribute_block"]}>
-            <h2 className={styles["attribute-label"]}>Carisma <span>{attributes?.charisma}</span> ({ calculateModifier(attributes?.charisma) })</h2>
+            <h2 className={styles["attribute-label"]}>Carisma <span>{attributes?.charisma}</span> ({ calculateModifier(attributes?.charisma || 0) })</h2>
           </div>
           <div className={styles["attribute_block"]}>
-            <h2 className={styles["attribute-label"]}>Destino <span>{attributes?.destiny}</span> ({ calculateModifier(attributes?.destiny) })</h2>
+            <h2 className={styles["attribute-label"]}>Destino <span>{attributes?.destiny}</span> ({ calculateModifier(attributes?.destiny || 0) })</h2>
           </div>
         </div>
 
