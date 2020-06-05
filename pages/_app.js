a4528adada8139d3873376a5a17737e18060a498
renatodex <renatodex@gmail.com>
2020-06-05 18:26:38 -0300

    Add stepper

--- pages/_app.js
+++ pages/_app.js
@@ -6,6 +6,11 @@ import 'pages/sheetgen/generator/breadcrumb'
 import Link from 'next/link'
 import Router from 'next/router';
 import NProgress from 'nprogress'; //nprogress module
+import Stepper from '@material-ui/core/Stepper';
+import Step from '@material-ui/core/Step';
+import StepLabel from '@material-ui/core/StepLabel';
+import { useRouter } from 'next/router'
+
 // import 'nprogress/nprogress.css'; //styles of nprogress
 //Binding events.
 Router.events.on('routeChangeStart', () => NProgress.start());
@@ -15,7 +20,50 @@ Router.events.on('routeChangeError', () => NProgress.done());
 
 // This default export is required in a new `pages/_app.js` file.
 export default function MyApp({ Component, pageProps }) {
-  console.log('Server', process.server)
+  const router = useRouter();
+
+  let steps = [
+    {
+      key: 0,
+      label: 'Escolher Nome',
+      path: '1-choose-name',
+    },
+    {
+      key: 1,
+      label: 'Escolher Raça',
+      path: '2-choose-race',
+    },
+    {
+      key: 2,
+      label: 'Escolher Classe',
+      path: '3-choose-class',
+    },
+    {
+      key: 3,
+      label: 'Escolher Atributos',
+      path: '4-choose-attributes',
+    },
+    {
+      key: 4,
+      label: 'Escolher Equipamentos',
+      path: '5-choose-equipments',
+    },
+    {
+      key: 5,
+      label: 'Escolher Habilidades',
+      path: '6-choose-spells',
+    },
+    {
+      key: 6,
+      label: 'Revisar',
+      path: 'review',
+    },
+  ]
+
+  let currentStep = steps.filter((step) => {
+    return router.pathname.includes(step.path)
+  })?.[0]?.key
+
   return (
     <div>
       <header>
@@ -36,7 +84,29 @@ export default function MyApp({ Component, pageProps }) {
       </header>
 
       <div className={'sheetGen'}>
-        <Component {...pageProps} />
+        { currentStep != null && (
+            <div className={'sheet-steps'}>
+              <Stepper activeStep={currentStep}>
+                {
+                  steps.map((step) => {
+                    return <Step key={step.label} completed={false}>
+                      <StepLabel
+                        optional={false}
+                      >
+                        {step.label}
+                      </StepLabel>
+                    </Step>
+                  })
+                }
+              </Stepper>
+            </div>
+          )
+        }
+
+
+        <div className={'sheetGen__block'}>
+          <Component {...pageProps} />
+        </div>
       </div>
     </div>
   )
